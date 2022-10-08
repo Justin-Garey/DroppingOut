c7fb648b933f46d936c89cd56fca30ba6d4c4de9
SilasSpringer <ss132517@ohio.edu>
2022-10-08 17:26:56 -0400

    enabled transcribe command to make bot join channel. currently breaks following this howver.

--- tr-tr-trevor/src/commands/transcribe.rs
+++ tr-tr-trevor/src/commands/transcribe.rs
@@ -1,10 +1,39 @@
-
+use serenity::async_trait;
+use serenity::client::Context;
+use serenity::model::id::GuildId;
 use serenity::builder::CreateApplicationCommand;
-use serenity::model::prelude::interaction::application_command::CommandDataOption;
+use serenity::model::prelude::interaction::application_command::{
+    CommandDataOption,
+    CommandDataOptionValue,
+};
 use serenity::model::prelude::command::CommandOptionType;
+use serenity::model::channel::{Channel, ChannelType};
 
-pub fn run(_options: &[CommandDataOption]) -> String {
-    "I am going to transcribe".to_string()
+// maybe make channel autoselection? aka. join the sending user's voice channel when unspecified
+// #[async_trait]
+pub async fn run(ctx: &Context, guild_id: GuildId, options: &[CommandDataOption]) -> String {
+    let option = options
+    .get(0)
+    .expect("Expected channel option")
+    .resolved
+    .as_ref()
+    .expect("Expected channel object");
+
+    let channel_id = 
+        if let CommandDataOptionValue::Channel(channel) = option {
+            match channel.kind {
+                ChannelType::Voice | 
+                ChannelType::Stage  => channel.id,//format!("{}'s id is {}", channel.name.as_ref().expect("expected channel to have a name."), channel.id),
+                _                   => return "Select a voice channel".to_string(),
+            }
+        } else {
+            return "Please provide a valid channel".to_string()
+        };
+    let manager = songbird::get(ctx).await
+        .expect("Songbird Voice client placed in at initialisation.").clone();
+    let _handler = manager.join(guild_id, channel_id).await;
+
+    return "made it".to_string();
 }
 
 pub fn register(command: &mut CreateApplicationCommand) -> &mut CreateApplicationCommand {
--- tr-tr-trevor/src/main.rs
+++ tr-tr-trevor/src/main.rs
@@ -1,8 +1,14 @@
 mod commands;
-
+// use crate::commands::*;
 use std::env;
 
 use songbird::SerenityInit;
+use serenity::client::Context;
+
+use serenity::{
+    client::{Client, EventHandler},
+    Result as SerenityResult,
+};
 
 use serenity::async_trait;
 use serenity::model::application::command::Command;
@@ -17,12 +23,19 @@ struct Handler;
 #[async_trait]
 impl EventHandler for Handler {
     async fn interaction_create(&self, ctx: Context, interaction: Interaction) {
+        let guild_id = GuildId(
+            env::var("GUILD_ID")
+                .expect("Expected GUILD_ID in environment")
+                .parse()
+                .expect("GUILD_ID must be an integer"),
+        );
+
         if let Interaction::ApplicationCommand(command) = interaction {
             // println!("Received command interaction: {:#?}", command);
 
             let content = match command.data.name.as_str() {
                 "ping" => commands::ping::run(&command.data.options),
-                "transcribe" => commands::transcribe::run(&command.data.options),
+                "transcribe" => commands::transcribe::run(&ctx, guild_id, &command.data.options).await,
                 "disconnect" => commands::disconnect::run(&command.data.options),
                 _ => "not implemented :(".to_string(),
             };
@@ -71,6 +84,7 @@ async fn main() {
     // Build our client.
     let mut client = Client::builder(token, GatewayIntents::empty())
         .event_handler(Handler)
+        .register_songbird()
         .await
         .expect("Error creating client");
 
