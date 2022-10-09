3bc910575140dadf2fd07855bc1e22fbb8bde8a2
Reilly Schultz <rpschultz1123@gmail.com>
2022-10-09 06:39:02 -0400

    Attempted to fix transcript printing

--- tr-tr-trevor/whisper_tr_tr.sh
+++ tr-tr-trevor/whisper_tr_tr.sh
@@ -11,16 +11,12 @@ while true; do
 		oldestfile="$files"
 		#"$( echo ${files} | head -1 )"
 		outfilename="$( echo ${oldestfile} | sed -E 's/[0-9]*_processing_([0-9]*)\.pcm/transcribed_\1/g' )"
-		outfileprevious="${outfilename}.msg"
-		messagefile="message.txt"
 
 		echo "Encoding . . ."
 		ffmpeg -y -f s16le -ar 96k -ac 1 -i ${oldestfile} "tmp.wav"
 
 		echo "Whispering . . ."
 		whisper "tmp.wav" --language ${language} --model ${model} >> ${outfilename}
-
-		cat ${outfilename} ${outfileprevious} | uniq > messagefile
 
 		echo "Cleaning up . . ."
 		rm ${oldestfile}
